a73f94013d516379205af121c8937ccd1f7751f1
Peter Gorniak <sumpfork@users.noreply.github.com>
2005-08-30 00:05:41 +0000

    take out another ok reference

--- neveredit/__init__.py
+++ neveredit/__init__.py
@@ -1,9 +1,7 @@
 """
 Neveredit aims to provide end-user module editing capabilities for
 Bioware's Neverwinter Nights role-playing game, which is sold without
-an editor on the Macintosh and Linux platforms. The project is part of
-the Openknights Consortium, a group of volunteers teaming up to provide
-tools for this great game.
+an editor on the Macintosh and Linux platforms.
 """
 
 __version__ = '0.8.1b1'
