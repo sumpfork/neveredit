91e3fce29accfe7814907600bdfee8df5499ef22
Peter Gorniak <sumpfork@users.noreply.github.com>
2005-09-04 23:43:34 +0000

    set default log level to WARN

--- neveredit/util/Loggers.py
+++ neveredit/util/Loggers.py
@@ -4,7 +4,7 @@ logger = logging.getLogger('neveredit')
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(levelname)s %(module)s: %(message)s'))
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARN)
 
 logger = logging.getLogger('neveredit.file')
 logger.setLevel(logging.WARN)
