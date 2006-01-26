d578b42526eb684381c2dd1a43e1d08e07795e2f
sumpfork <sumpfork@b8c5ca63-dd0d-0410-aac7-9211b2d1b731>
2006-01-26 23:10:31 +0000

    compress some UI controls to make properties page more compact

--- trunk/neveredit/ui/NeverEditMainApp.py
+++ trunk/neveredit/ui/NeverEditMainApp.py
@@ -115,7 +115,7 @@ class NeverEditMainWindow(wx.Frame,PropertyChangeListener):
         
         self.splash.Show(True)
 
-        wx.Frame.__init__(self,parent,-1,title,size=(840,600),\
+        wx.Frame.__init__(self,parent,-1,title,size=(820,600),\
                           style=wx.DEFAULT_FRAME_STYLE\
                           |wx.NO_FULL_REPAINT_ON_RESIZE)
 
